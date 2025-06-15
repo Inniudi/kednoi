468df3ffb4afb2542cc53e4282b25fff6a0a966e
JJ <50021740+Inniudi@users.noreply.github.com>
2025-06-15 10:24:54 +0200

    Update characterEditor.js

--- js/characterEditor.js
+++ js/characterEditor.js
@@ -31,7 +31,7 @@ function UpdateCharacterList()
     {
         let mainInfo = cards[i].getElementsByClassName("charMainInfo")[0];
         characters.push(new Character(mainInfo.getElementsByClassName("charName")[0].textContent, mainInfo.getElementsByClassName("charAge")[0].textContent));
-        mainInfo.getElementsByClassName("charDialogPercent")[0].textContent = CalcDialogPercent(mainInfo.getElementsByClassName("charName")[0].textContent) + "% del diálogo";
+        if(editorType === "SP") mainInfo.getElementsByClassName("charDialogPercent")[0].textContent = CalcDialogPercent(mainInfo.getElementsByClassName("charName")[0].textContent) + "% del diálogo";
     }
 }
 
